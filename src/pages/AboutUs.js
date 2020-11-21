38bd9889261ffdd06372a7e961505da5edb58e38
Sean <seanhb714@gmail.com>
2020-11-21 11:22:17 -0800

    112120-Faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,15 @@
 //COMPONENTS
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 
 const AboutUs = () => {
   return(
+    <>
     <AboutSection />
+    <ServicesSection />
+    <FaqSection />
+    </>
   )
 }
 
